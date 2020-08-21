4ee3a892d74983c688b7460cff5272ca58fa3341
eg-goeppert <goeppert@energate.de>
2020-08-21 18:07:31 +0200

    Evaluate MCTS aganst flatMC

---
+++ auswertungen/mcts/vs_random.py
@@ -0,0 +1,13 @@
+from bachelorarbeit.mcts import MCTSPlayer
+from bachelorarbeit.tools import evaluate_against_random, dump_json
+
+if __name__ == "__main__":
+
+    results = evaluate_against_random(
+        player=MCTSPlayer,
+        num_games=100,
+        num_processes=6
+    )
+    print(results)
+
+    dump_json("data/vs_random_{}.json", results)
