03ca68e41efc89f8fc6a0a3bfff487e2235387d2
sgoeppert <stefan.goeppert@gmail.com>
2020-08-25 18:04:31 +0200

    Long running Transpos test

--- run_transpos_keep_tree.py
+++ run_transpos_keep_tree.py
@@ -1,25 +1,28 @@
 from bachelorarbeit.transposition import TranspositionPlayer
 from bachelorarbeit.mcts import MCTSPlayer
-from bachelorarbeit.tools import run_selfplay_experiment
+from bachelorarbeit.tools import run_selfplay_experiment, dump_json
 
 
 if __name__ == "__main__":
 
     MAX_STEPS = 10000
-    UCT_METHOD = "default"
+    METHODS = ["UCT1", "UCT2", "UCT3", "Standard"]
     EXPLORATION_CONSTANT = 0.5
     MCTS_CP = 0.85
 
-    # 5000 default - Keep tree both:
-    res = run_selfplay_experiment(
-        "Transpos vs MCTS, keeping trees",
-        players=(TranspositionPlayer, MCTSPlayer),
-        constructor_args=(
-            {"max_steps": MAX_STEPS, "keep_tree": True, "uct_method": UCT_METHOD, "exploration_constant": EXPLORATION_CONSTANT},
-            {"max_steps": MAX_STEPS, "keep_tree": True, "exploration_constant": MCTS_CP}
-        ),
-        num_games=300,
-        num_processes=10,
-        show_progress_bar=True
-    )
+    results = {"max_steps": MAX_STEPS, "Cp": EXPLORATION_CONSTANT}
+    for method in METHODS:
+        res = run_selfplay_experiment(
+            "Transpos vs MCTS, keeping trees",
+            players=(TranspositionPlayer, MCTSPlayer),
+            constructor_args=(
+                {"max_steps": MAX_STEPS, "keep_tree": True, "uct_method": method, "exploration_constant": EXPLORATION_CONSTANT},
+                {"max_steps": MAX_STEPS, "keep_tree": True, "exploration_constant": MCTS_CP}
+            ),
+            num_games=500,
+            show_progress_bar=True
+        )
+        results[method] = res.mean
+
+    dump_json("long_runs/transposition_{}.json", results)
 
