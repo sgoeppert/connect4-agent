407fe82e4d53709b8f5f568638bd66087b022741
sgoeppert <stefan.goeppert@gmail.com>
2020-10-03 19:04:09 +0200

    Remove Flat Monte Carlo and add the actTimeout to the first turn

--- bachelorarbeit/players/base_players.py
+++ bachelorarbeit/players/base_players.py
@@ -41,6 +41,7 @@ class TreePlayer(Player):
         self.max_steps = max_steps
         self.steps_taken = 0
         self.initialized = False
+
         # Limitiert die Ausführungszeit
         self.start_time = 0
         self.time_limit = 0
@@ -57,7 +58,7 @@ class TreePlayer(Player):
             if self.initialized:
                 limit = conf.actTimeout
             else:
-                limit = conf.agentTimeout
+                limit = conf.agentTimeout + conf.actTimeout
 
             if limit > 0:
                 self.time_limit = limit * (1-self.time_buffer_pct)
@@ -126,68 +127,3 @@ class TreePlayer(Player):
         self._store_root(root.children[best])
 
         return best
-
-def simulate_game(game):
-    while not game.is_terminal():
-        move = random.choice(game.list_moves())
-        game.play_move(move)
-    return game
-
-class FlatMonteCarlo(Player):
-    name = "FlatMonteCarloPlayer"
-
-    def __init__(self,
-                 max_steps: int = 1000,
-                 exploration_constant: float = 0.8,
-                 ucb_selection: bool = True,
-                 **kwargs):
-
-        super(FlatMonteCarlo, self).__init__(**kwargs)
-        self.max_steps = max_steps
-        self.ucb_selection = ucb_selection
-        self.c_p = exploration_constant
-
-    def get_move(self,
-                 observation: Observation,
-                 configuration: Configuration) -> int:
-
-        game = ConnectFour(board=observation.board,
-                           rows=configuration.rows,
-                           columns=configuration.columns,
-                           inarow=configuration.inarow,
-                           mark=observation.mark)
-
-        n = defaultdict(int)
-        q = defaultdict(float)
-        scoring_player = game.get_current_player()
-
-        def ucb(m, N=1):
-            return q[m] + self.c_p * math.sqrt(math.log(N) / max(1,n[m]))
-
-        for i in range(1, self.max_steps+1):
-            if self.ucb_selection:
-                a = max(game.list_moves(), key=lambda m: ucb(m, i))
-            else:
-                a = random.choice(game.list_moves())
-
-            end_state = simulate_game(game.copy().play_move(a))
-            r = end_state.get_reward(scoring_player)
-            n[a] += 1
-            q[a] += (r - q[a])/n[a]
-
-        return max(n.keys(), key=lambda m: ucb(m))
-
-
-if __name__ == "__main__":
-    from bachelorarbeit.selfplay import Arena
-    from bachelorarbeit.tools import timer
-
-    steps = 20
-    arena = Arena(
-        players=(FlatMonteCarlo, RandomPlayer),
-        constructor_args=({"max_steps": steps, "exploration_constant": 1 / math.sqrt(2)},
-                          None),
-        num_games=100,
-        num_processes=4
-    )
-    results = arena.run_game_mp()
