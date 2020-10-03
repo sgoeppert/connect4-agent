b866ef0c4f6eb355b03a075a5ffd0bcef529c842
sgoeppert <stefan.goeppert@gmail.com>
2020-10-03 20:08:48 +0200

    Add file that created training data through selfplay

---
+++ generate_selfplay_training_data.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    from bachelorarbeit.selfplay import Arena, Memory
+    from bachelorarbeit.players.mcts import MCTSPlayer
+
+    memory = Memory("selfplay_mcts_strong_v2.pickle")
+    arena = Arena(players=(MCTSPlayer, MCTSPlayer),
+                  num_games=500,
+                  num_processes=10,
+                  memory=memory)
+
+    iterations = 50
+
+    for i in range(iterations):
+        arena.run_game_mp()
+        memory.save_data()
+        print(memory.num_states, "states in memory")
